df6ce05e871199ecea1aa51c1ef2a3a455123a0b
paynej5504 <54283576+paynej5504@users.noreply.github.com>
2019-10-02 14:25:13 -0400

    Update HelloTest1.java
    I added an additional comment on line three and modified the comment on line nine.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/2/19 by JLP
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request.
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
